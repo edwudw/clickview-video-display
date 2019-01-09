be39086b9f86358ffe119bd5ec7f0d833455a60e
Edward Webb <spider.pole@live.com>
2019-01-09 14:35:13 +1100

    Displayed videos stored in videos.js in a table

---
+++ displayVideos.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+	console.log("Running!");
+	// Load videos into table
+	// Loop through videos array created by videos.js and add each to the table
+	console.log(videos);
+	for (let i = 0; i < videos.length; i++) {
+		addToTable(videos[i]);
+	}
+})
+
+// Adds each video to the table displayed
+function addToTable(video) {
+	$tableBody = $("#displayTableBody");
+	$tableBody.append("<tr>\
+					     <td><img class='thumbnail' src=" + video['thumbnail'] + " alt='Video Thumbnail'></td>\
+					     <td>" + video['name'] + "</td>\
+					     <td>" + video['description'] + "</td>\
+					     <td><button type='button' class='btn btn-success'>Play</button></td>\
+					   </tr>");
+
+}
